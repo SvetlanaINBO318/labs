3b2f1faa8dfd5dbe5a1da1c755c85185551f4c9c
SvetlanaINBO318 <47588192+SvetlanaINBO318@users.noreply.github.com>
2019-03-30 20:28:44 +0300

    Create Complex.cpp

---
+++ laba1/Complex.cpp
@@ -0,0 +1,30 @@
+#include "Complex.h"
+
+
+Complex sum(Complex a, Complex b) {
+        Complex result;
+        result.re = a.re + b.re;
+        result.im = a.im + b.im;
+        return result;
+}
+
+Complex raz(Complex a, Complex b) {
+        Complex result;
+        result.re = a.re - b.re;
+        result.im = a.im - b.im;
+        return result;
+}
+
+Complex umn(Complex a, Complex b) {
+        Complex result;
+        result.re = a.re*b.re - a.im*b.im;
+        result.im = a.re * b.im + b.re * a.im;
+        return result;
+}
+
+Complex del(Complex a, Complex b) {
+        Complex result;
+        result.re = (a.re * b.re + a.im * b.im) / (pow(b.re, 2) + pow(b.im, 2));
+        result.im = (b.re * a.im - a.re * b.im) / (pow(b.re, 2) + pow(b.im, 2));
+        return result;
+}
