b2c167587cd6f6dc9db656e0dbbe6c7e290f8dbb
SvetlanaINBO318 <47588192+SvetlanaINBO318@users.noreply.github.com>
2019-03-30 20:26:51 +0300

    Create Complex.h

---
+++ laba1/Complex.h
@@ -0,0 +1,18 @@
+#ifndef CO_H
+#define CO_H
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+class Complex {
+public:
+        float re;
+        float im;
+};
+
+Complex sum(Complex a, Complex b);// plus
+Complex raz(Complex a, Complex b);// minus
+Complex umn(Complex a, Complex b);// umnoz
+Complex del(Complex a, Complex b);// delenie
+
+#endif
