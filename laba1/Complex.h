9ea38ef810c26248c36f24c2c91d641d1a995e5c
SvetlanaINBO318 <47588192+SvetlanaINBO318@users.noreply.github.com>
2019-03-30 20:30:33 +0300

    Update Complex.h

--- laba1/Complex.h
+++ laba1/Complex.h
@@ -14,5 +14,6 @@ Complex sum(Complex a, Complex b);// plus
 Complex raz(Complex a, Complex b);// minus
 Complex umn(Complex a, Complex b);// umnoz
 Complex del(Complex a, Complex b);// delenie
+float abs(Complex a);//modul
 
 #endif
